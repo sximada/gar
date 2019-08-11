64a3e063c231d929c7b5e5e291e29c214440975e
sximada <50688746+sximada@users.noreply.github.com>
2019-08-11 18:44:15 +0900

    Add service pingpong

--- service-pingpong/service_pingpong/__init__.py
+++ service-pingpong/service_pingpong/__init__.py
@@ -1,1 +1,26 @@
+#! /usr/bin/env python3
+from flask import Flask
+
 __version__ = '0.1.0'
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return """<!DOCTYPE html>
+<html>
+    <head>
+	<title>Flask Ping Pong Server</title>
+    </head>
+    <body>
+	<h1>Flask Ping Pong Server</h1>
+	<p>
+	    Call <a href="/ping/">Ping Pong API</a>.
+	</p>
+    </body>
+</html>
+"""
+
+@app.route("/ping/")
+def ping():
+    return "PONG"
